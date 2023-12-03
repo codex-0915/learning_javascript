b26801a47b6d7946349595748f8b513412432cf1
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-03 19:44:33 +0800

    Add concepts about variables

---
+++ complete_path_to_javascript_mastery/002_variables_and_data_types/script.js
@@ -0,0 +1,18 @@
+/* Variables */
+
+// Creating variable using var keyboard
+var varVariableName = 'Welcome to variables';
+console.log(varVariableName);
+
+// Creating variable using let keyboard
+let letVariableName = 'Test';
+console.log(letVariableName);
+
+/* 
+Variable Naming Rules
+1. The name should be unique
+2. The name should not be any reserved JS keyword
+3. The name must start with a character 
+   (alphanumeric keyword), _, and &
+*/
+
