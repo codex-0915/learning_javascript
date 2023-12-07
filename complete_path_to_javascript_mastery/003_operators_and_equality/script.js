40ff16db11c516a4d416f1c15893897226112cd0
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-07 21:22:01 +0800

    Add practice codes for arithmetic operators in JS

---
+++ complete_path_to_javascript_mastery/003_operators_and_equality/script.js
@@ -0,0 +1,20 @@
+/* Arithmetic Operators */
+const num1 = 124;
+const num2 = 3;
+let result = 0;
+// Addition
+result = num1 + num2;
+// Subtraction
+result = num1 - num2;
+// Multiplication
+result = num1 * num2;
+// Division
+result = num1 / num2;
+// Exponent
+result = num1 ** num2;
+// Modulo
+result = num1 % num2;
+// Increment & Decrement
+result++;
+result--;
+console.log(result);
