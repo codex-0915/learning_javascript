3b864d321949b4c00bb14066933d1d8a85a7f62a
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-08 22:17:41 +0800

    Play around with if-else conditional statement in JS

---
+++ complete_path_to_javascript_mastery/004_logic_and_control_flow/script.js
@@ -0,0 +1,12 @@
+/* If - Else Statements */
+const age = 18;
+
+if (age > 18) {
+    console.log("Age is greater than 18. You're welcome here!");
+}
+if (age == 18) {
+    console.log("You just turned 18. WELCOME!");
+}
+else {
+    console.log("You are not allowed here!");
+}
