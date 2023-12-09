4fcc6509c47f5a9a4bb07c1687ad794bf0d17e56
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-09 10:50:13 +0800

    Play around with switch conditional statement in JS

--- complete_path_to_javascript_mastery/004_logic_and_control_flow/script.js
+++ complete_path_to_javascript_mastery/004_logic_and_control_flow/script.js
@@ -39,3 +39,21 @@ if (isCool && age2 > 18) {
 
 console.log('truthy' && 0 && 'test' && 999);
 console.log('truthy' || 0 || 'test' || 999);
+
+/* Switch Statements */
+const superHero = "Batman";
+
+switch(superHero) {
+    case 'Iron Man':
+        console.log("I am Iron Man!");
+    break;
+    case 'Thor':
+        console.log("That is my hammer...");
+    break;
+    case 'Black Widow':
+        console.log('One shot, one kill...');
+    break;
+    default:
+        console.log("Enter a valid superhero name!");
+    break;
+}
