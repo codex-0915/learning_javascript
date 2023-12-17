72cd39a82736e2b28651ff216547e63686262d81
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-17 10:33:25 +0800

    Add practice codes for declaring and invoking functions in JS

--- complete_path_to_javascript_mastery/005_functions/script.js
+++ complete_path_to_javascript_mastery/005_functions/script.js
@@ -8,6 +8,31 @@ function square(number) {
     return number * number;
 }
 
+/* DECLARING FUNCTIONS IN JS */
 // FUNCTION CALL (calling/executing a function)
 const result = square(5);
 console.log(result);
+
+// Function declaration
+function name(params) {
+    // statements
+    // have access to "this" keyword
+}
+
+// Function expression
+const name2 = function(params) {
+    // statements
+}
+
+// Arrow function
+const name3 = (params) => {
+    // statements
+}
+
+/* Invoking Functions */
+function sayHi(name) {
+    console.log(`Hi, ${name}!`);
+}
+
+sayHi('Joe');
+sayHi('Jane');
