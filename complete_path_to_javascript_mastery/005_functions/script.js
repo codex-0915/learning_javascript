30280da20fd3a02714efda5651b05e4f3f382c20
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-17 10:17:26 +0800

    Add practice codes for learning basics of functions in JS

---
+++ complete_path_to_javascript_mastery/005_functions/script.js
@@ -0,0 +1,13 @@
+/* 
+Functions
+    A block of code => PERFORMS A TASK
+*/
+
+// FUNCTION DECLARATION (defining a function)
+function square(number) {
+    return number * number;
+}
+
+// FUNCTION CALL (calling/executing a function)
+const result = square(5);
+console.log(result);
