37162bb273dec9a04ca879888ba4996cb21aa23f
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-22 00:26:02 +0800

    Add practice codes for understanding scopes in JS

--- complete_path_to_javascript_mastery/006_tricky_parts/script.js
+++ complete_path_to_javascript_mastery/006_tricky_parts/script.js
@@ -1,3 +1,28 @@
 /* 
 Tricky Parts
 */
+
+// Global Scope
+let name = 'John';
+const logName = () => {
+    console.log(name);
+
+    name = 'Jane';
+}
+logName();
+console.log(name); // This shows the disadvantage of global variable
+
+// Local Scope / Function Scope
+const someFunction = () => {
+    const name2 = 'Myk';
+    console.log(name2);
+}
+someFunction();
+console.log(name2); // This would cause error
+
+// Block Scope
+if(true) {
+    const name3 = 'John';
+    console.log(name3); // This will work
+}
+console.log(name3); // name3 can't be found
