3699ae39fea6aa359122e68b03a5ae3567acdd4c
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2023-12-28 13:53:07 +0800

    Add practice codes for basic concept of strings in JS

--- complete_path_to_javascript_mastery/007_strings_in_detail/script.js
+++ complete_path_to_javascript_mastery/007_strings_in_detail/script.js
@@ -1,3 +1,23 @@
 /* 
 String in Details
 */
+
+// Introduction to Strings
+const single = 'This is a string';
+const double = "This is a string";
+
+const sum = (a,b) => a + b;
+const total = `The sum is ${sum(2, 10)}.`; // Backticks are dynamic
+const rows = `1
+2
+3
+`
+const greeting = 'Hi, I\'m John, \"Johnny John\".';
+const greeting2 = `Hi, I'm John, "Johnny John".`;
+
+console.log(single);
+console.log(double);
+console.log(total);
+console.log(rows);
+console.log(greeting);
+console.log(greeting2);
